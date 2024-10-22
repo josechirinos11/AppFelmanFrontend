e013e5aba97945a23b6840dfe7f7fd0490df79fd
josechirinos11 <josechirinos11@gmail.com>
2024-10-22 19:53:50 +0200

    Inicio de la aplicacion con solamente Rutas del layout

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,15 @@
+import {  Outlet } from 'react-router-dom'
+
+
+export default function AppLayout() {
+    return (
+        <>
+            <div>AppLayout</div>
+
+            <Outlet />
+
+        </>
+       
+       )
+        
+}
---
+++ src/views/DashboardView.tsx
@@ -0,0 +1,12 @@
+
+
+
+export default function DashboardView() {
+
+
+ return (
+  <div>DashboardView</div>
+ )
+  
+  
+}
