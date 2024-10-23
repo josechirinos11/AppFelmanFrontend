9b75c2fe959caf9d9189193092bc7081408def21
josechirinos11 <josechirinos11@gmail.com>
2024-10-23 21:32:26 +0200

    registro usuario sin terminar coneccion base de datos

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -4,9 +4,9 @@ import {  Outlet } from 'react-router-dom'
 export default function AppLayout() {
     return (
         <>
-            <div>AppLayout</div>
-
+            <main className="container mx-auto md:grid md:grid-cols-2 mt-12 gap-10 p-5 items-center">
             <Outlet />
+            </main>
 
         </>
        
