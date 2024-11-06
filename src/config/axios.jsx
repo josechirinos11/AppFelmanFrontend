e9e211c9d3d61fae195c13c18382139ad1fd4797
josechirinos11 <josechirinos11@gmail.com>
2024-11-06 20:05:16 +0100

    configuraciones para carga en produccion con vercel

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -1,7 +1,9 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const clienteAxios = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/felman`
-})
+    baseURL: import.meta.env.DEV 
+        ? import.meta.env.VITE_BACKEND_URL_LOCAL 
+        : import.meta.env.VITE_BACKEND_URL_PROD
+});
 
-export default clienteAxios
+export default clienteAxios;
